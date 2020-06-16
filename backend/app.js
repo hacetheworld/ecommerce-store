83007dc833eb36c68e06294ff808770e8e190797
Ajay <majay1638@gmail.com>
2020-06-16 09:26:33 +0530

    make it better

--- backend/app.js
+++ backend/app.js
@@ -27,9 +27,7 @@ if (process.env.NODE_ENV !== "production") {
   app.use(express.static("../frontend/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(
-      path.join(__dirname, "../", "frontend", "build", "index.html")
-    );
+    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
   });
 }
 // listen to the port
